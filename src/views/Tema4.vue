3d50b8485fc32faf7e847dabd413d7dbe2482eb2
AdonadoMolinares <donadomolinares@gmail.com>
2026-07-23 10:48:51 -0500

    Commit

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -454,6 +454,21 @@
               i.fa-solid.fa-circle.me-3.flex-shrink-0(style="color:#FF8E6B; font-size: 8px; line-height: 1.5;").mb-0
               span En la planeación: se utilizan para formular acciones, estrategias y procesos de seguimiento. 
     
+    p.mb-3 Lo invitamos a escuchar el siguiente pódcast.
+    .row.justify-content-center.mb-5(data-aos="zoom-in-right")
+      .col-12.col-lg-8
+        .tarjeta.color-primario.p-5.rounded-3.mb-3
+          .row.justify-content-center.align-items-center.mb-0
+            .col-8.col-md-6.col-lg-6.mb-lg-0.mb-3
+              figure
+                img(src="@/assets/curso/tema2/img_20.png", class="img-fluid w-100")
+            .col-12.col-lg-6.mb-lg-0
+              h2.mb-5.text-white Pódcast
+              TarjetaAudio.color-acento-botones.mb-0.p-5(
+                texto="<I>Organización y participación: bases para fortalecer las comunidades rurales</I>"
+                :audio="require('@/assets/curso/podcast/podcast_2.mp3')"
+                @audio-hover="mostrarIndicadorTarjetaAudio = True")
+                .indicador--click(v-if="mostrarIndicadorTarjetaAudio")
    
 </template>
 
